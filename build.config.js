15a28d94da5c881ff29296954611a1712ead8e68
HenryC-3 <2324375439@qq.com>
2023-02-09 13:02:09 +0800

    build: update build script

--- build.config.js
+++ build.config.js
@@ -1,16 +1,37 @@
 import * as esbuild from 'esbuild'
+import { remove } from 'fs-extra'
+import { writeFile } from "fs/promises"
 
-export const ctx = await esbuild.context({
-    entryPoints: ['./src/scripts/index.ts', "./src/cli/index.ts"],
-    outdir: "./output",
-    outExtension: { '.js': '.cjs' }, // [esbuild - API](https://esbuild.github.io/api/#out-extension)
-    bundle: true,
-    minify: true,
-    platform: 'node',
-    sourcemap: true,
-    target: 'node18',
-    format: "cjs",
-})
+
+export const ctx = await esbuild.context(await getConfig())
+
+// build
+const result = await esbuild.build(await getConfig())
+// create meta.json for bundle analysis  [esbuild - Bundle Size Analyzer](https://esbuild.github.io/analyze/)
+await writeFile('meta.json', JSON.stringify(result.metafile))
+process.exit()
+
+async function getConfig() {
+    const env = process.env.NODE_ENV
+    const defaultConfig = {
+        entryPoints: ['./src/scripts/index.ts', "./src/cli/index.ts"],
+        outdir: "./output",
+        outExtension: { '.js': '.cjs' }, // [esbuild - API](https://esbuild.github.io/api/#out-extension)
+        bundle: true,
+        minify: true,
+        platform: 'node',
+        sourcemap: true,
+        metafile: true,
+        target: 'node18',
+        format: "cjs",
+    }
+    if (env === "production") {
+        await remove("output")
+        const productionConfig = Object.assign(defaultConfig, { sourcemap: false })
+        return productionConfig
+    }
+    return defaultConfig
+}
 
 
 // TODO dynamically generate vscode debug configuration in launch.json. waiting for API to implement this, see [List all command's arguments using Helper · Issue #1823 · tj/commander.js](https://github.com/tj/commander.js/issues/1823)
