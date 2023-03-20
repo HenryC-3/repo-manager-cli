da5368bbe24ac26ebe3ba49f1b6cc7f0db41233c
HenryC-3 <2324375439@qq.com>
2023-03-20 11:54:49 +0800

    build: integrate semantic-release

---
+++ .releaserc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    branches: ["master"],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "angular",
+                releaseRules: [
+                    { type: "docs", release: "patch" },
+                    { type: "refactor", release: "patch" },
+                    { type: "style", release: "patch" },
+                    { type: "chore", release: "patch" },
+                    { type: "build", release: "minor" },
+                ],
+                parserOpts: {
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+                },
+            },
+        ],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+        "@semantic-release/npm",
+    ],
+};
