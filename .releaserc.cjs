f0533eba480f1065e6cfa895cd98839ce869b4fc
HenryC-3 <2324375439@qq.com>
2023-03-20 12:21:35 +0800

    build: autoudpate version in package.json

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -20,5 +20,13 @@ module.exports = {
         "@semantic-release/release-notes-generator",
         "@semantic-release/github",
         "@semantic-release/npm",
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json"],
+                message:
+                    "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
     ],
 };
